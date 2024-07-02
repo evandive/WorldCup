967fd5d6f90149416783c5381571911155b9d4e8
evandive <emmabean@icloud.com>
2024-07-02 14:48:53 -0400

    Add files via upload

---
+++ insert_data.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+if [[ $1 == "test" ]]
+then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
+
+# Do not change code above this line. Use the PSQL variable above to query your database.
+echo $($PSQL "truncate games, teams;")
+
+cat games.csv | while IFS="," read year round winner opponent winner_goals opponent_goals
+do
+if [[ $year != year ]]
+then
+  winner_id=$($PSQL "select team_id from teams where name='$winner'")
+  if [[ -z $winner_id ]]
+  then
+  insert_winner_result=$($PSQL "insert into teams(name) values('$winner');")
+  winner_id=$($PSQL "select team_id from teams where name='$winner'")
+  fi
+  opp_id=$($PSQL "select team_id from teams where name='$opponent'")
+  if [[ -z $opp_id ]]
+  then
+  insert_opp_result=$($PSQL "insert into teams(name) values('$opponent');")
+  opp_id=$($PSQL "select team_id from teams where name='$opponent'")
+  fi
+   insert_games_result=$($PSQL "insert into games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) values($year, '$round', $winner_id, $opp_id, $winner_goals, $opponent_goals);")
+fi
+done
